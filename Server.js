3fc578b9f35333b853b1ef10182aec326abdb450
Aaron RJ <aresinodeveloper@gmail.com>
2019-05-07 21:12:32 +0200

    Main commit

--- Server.js
+++ Server.js
@@ -1,8 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + 'dist'));
-// Start the app by listening on the default
-// Heroku port
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/superheroes'));
+
+app.get('/*', function (req, res) {
+
+  res.sendFile(path.join(__dirname + '/dist/superheroes/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
